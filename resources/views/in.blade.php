2857494929d1b668136878cb438d84ff59399465
turrud <fatur14022@gmail.com>
2023-10-20 11:03:41 +0700

    clear

--- resources/views/in.blade.php
+++ resources/views/in.blade.php
@@ -28,7 +28,6 @@
 
     <h1>Under Construction</h1>
     <p>We're working on something awesome. Please check back later.</p>
-   ]
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   </body>
